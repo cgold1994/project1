44238a3dedeaf7d00acf90a9fff230ef41d5f307
Goldsmith <cierra.goldsmith@pearson.com>
2022-03-11 11:25:22 -0500

    First input field made with attributes/button

---
+++ script.js
@@ -0,0 +1,9 @@
+// const answer1= document.getElementById('easy');
+// const btn1= document.getElementById('btn1');
+// const out1= document.getElementById('output1');
+
+// function level1() {
+//     out1.innerHTML = answer1.value;
+// }
+
+// btn1.addEventListener('click',level1);
