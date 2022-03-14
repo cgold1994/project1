ee7fe95dbd86bab98734ca2698ccd56abefe6af5
Goldsmith <cierra.goldsmith@pearson.com>
2022-03-13 21:52:41 -0400

    value from input fields working after submit

--- script.js
+++ script.js
@@ -15,3 +15,53 @@ myCounter = setInterval(function(){
 };
 
 startEl.addEventListener('click', handleStartClick);
+
+// Declare variables for input fields
+const easyOne = document.getElementById("myText1");
+const mediumOne = document.getElementById("myText2");
+const hardOne = document.getElementById("myText3");
+const bonusOne = document.getElementById("myText4");
+
+const buttonOne = document.getElementById("btn1");
+const buttonTwo = document.getElementById("btn2");
+const buttonThree = document.getElementById("btn3");
+const buttonFour = document.getElementById("btn4");
+
+const out1 = document.getElementById("word1");
+const out2 = document.getElementById("word2");
+const out3 = document.getElementById("word3");
+const out4 = document.getElementById("word4");
+
+function fun1(e) {
+    e.preventDefault();
+    out1.innerHTML = easyOne.value;
+
+}
+
+function fun2(e) {
+    e.preventDefault();
+    out2.innerHTML = mediumOne.value;
+
+}
+
+function fun3(e) {
+    e.preventDefault();
+    out3.innerHTML = hardOne.value;
+
+}
+
+function fun4(e) {
+    e.preventDefault();
+    out4.innerHTML = bonusOne.value;
+
+}
+
+
+btn1.addEventListener('click', fun1);
+btn2.addEventListener('click', fun2);
+btn3.addEventListener('click', fun3);
+btn4.addEventListener('click', fun4);
+
+
+
+
